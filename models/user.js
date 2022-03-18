83977aa9ee356940f1260af83b063342a3758327
hijal <hijikeshhijal@gmail.com>
2022-03-18 12:24:31 +0600

    update slug

--- models/user.js
+++ models/user.js
@@ -1,38 +1,41 @@
-const SequelizeSlugify = require('sequelize-slugify');
-const Joi = require('joi');
-const { useBcrypt } = require('../utils/bcrypt.password');
+const SequelizeSlugify = require("sequelize-slugify");
+const Joi = require("joi");
+const { useBcrypt } = require("../utils/bcrypt.password");
 
 module.exports = (sequelize, DataTypes) => {
-	const User = sequelize.define('user', {
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-		username: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			schema: Joi.string().min(4).required(),
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			schema: Joi.string().min(8).required(),
-		},
-		slug: {
-			type: DataTypes.STRING,
-			unique: true,
-		},
-	});
+  const User = sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      schema: Joi.string().min(4).required(),
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      schema: Joi.string().min(8).required(),
+    },
+    slug: {
+      type: DataTypes.VIRTUAL,
+      unique: true,
+      get() {
+        return `${this.get("username")} ${this.get("id")}`.replace(/\s/g, "-");
+      },
+    },
+  });
 
-	SequelizeSlugify.slugifyModel(User, {
-		source: ['username'],
-	});
+  SequelizeSlugify.slugifyModel(User, {
+    source: ["username"],
+  });
 
-	useBcrypt(User, {
-		rounds: 10,
-		compare: 'authenticate',
-	});
+  useBcrypt(User, {
+    rounds: 10,
+    compare: "authenticate",
+  });
 
-	return User;
+  return User;
 };
